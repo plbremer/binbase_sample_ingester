347afeb71c6d9f478d6bb4442f38a3b804d5bb39
rictuar <parkerbremer@gmail.com>
2022-12-01 18:58:06 -0800

    fix memory leak and event['body']

--- lambda_tests/write_lambda/app.py
+++ lambda_tests/write_lambda/app.py
@@ -10,35 +10,38 @@ import json
 
 s3_client = boto3.client("s3")
 
+#read jsons and new values
+#jsons
+temp_response = s3_client.get_object(Bucket='parkerslambdatest', Key='training_set_list.json')
+tfidf_vectorizer_training_set = json.load(
+    temp_response['Body']#.read().decode('utf-8')
+)
+temp_response = s3_client.get_object(Bucket='parkerslambdatest', Key='customer_added.json')
+customer_added = json.load(
+    temp_response['Body']#.read().decode('utf-8')
+)
+
+
 
 def handler(event, context):
 
 
 
-    #read jsons and new values
-    #jsons
-    temp_response = s3_client.get_object(Bucket='parkerslambdatest', Key='training_set_list.json')
-    tfidf_vectorizer_training_set = json.load(
-        temp_response['Body']#.read().decode('utf-8')
-    )
-    temp_response = s3_client.get_object(Bucket='parkerslambdatest', Key='customer_added.json')
-    customer_added = json.load(
-        temp_response['Body']#.read().decode('utf-8')
-    )
+
     #new values - make as a dict in case we want to encapuslate the "column" that the value came from in the excel file
     customer_added_values_keys=list(event['customer_added_values'].keys())
 
     #increase customer_added json with whatever is fresh
     customer_added.update(event['customer_added_values'])
     #increase training list by whatever is fresh
-    tfidf_vectorizer_training_set=tfidf_vectorizer_training_set+customer_added_values_keys
+    augmented_tfidf_vectorizer_training_set=tfidf_vectorizer_training_set+customer_added_values_keys
     
 
     #train new  models
     my_TfidfVectorizer=TfidfVectorizer(
             analyzer=trigrams,
         )
-    tfidf_matrix=my_TfidfVectorizer.fit_transform(tfidf_vectorizer_training_set)
+    tfidf_matrix=my_TfidfVectorizer.fit_transform(augmented_tfidf_vectorizer_training_set)
     NN_model=NearestNeighbors(
         n_neighbors=40,
         n_jobs=-1,
